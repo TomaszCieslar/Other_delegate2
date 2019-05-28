209a7fa27644986e3b2ed683968cb64797ac757b
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-05-28 09:50:25 +0200

    Add project files.

---
+++ Other_delegate2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Other_delegate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
